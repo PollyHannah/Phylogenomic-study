18481dff5fcd249ca8a469432e6ee0ad47e4e116
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-06-05 18:44:37 +1000

    Update Script_One_prokka.sh

--- Script_One_prokka.sh
+++ Script_One_prokka.sh
@@ -6,11 +6,16 @@ Cd Desktop/MCV_pipeline
 # Create a directory to store the Prokka outputs
 Mkdir prokka_outputs
 
-# Navigate to the directory with the pork input .fasta files
+# Navigate to the directory with the input .fasta files. Prokka automatically uses the files contained in the directory you run thr program from, as input.
 Cd genomes/
 
-#Set up loop to go through each FASTA file and run prokka 
-For file in *.fasta; do
+#Define 'file' as an array
+#basename is a command used to strip suffix information from a filename, which leaves only the file name itself.
+File= (`basename ${input} .fasta`) 
 
-#Script to run prokkka on all megalocytivirus and iridovirus genomes 
-% prokka --kingdom viruses --outdir /Desktop/MCV_pipeline/Prokka_outputs
+#Set up loop to go through each FASTA file and run prokka 
+For File in *.fasta; do
+
+#Script to run prokkka on all megalocytivirus and iridovirus genomes
+#If an out directory using --outdir isn't specified Prokka will put the out directory in the same directory as the input files 
+% prokka --kingdom viruses --outdir /Desktop/MCV_pipeline/Prokka_outputs/${File[@]} --prefix ${File[@]} --locustag ${file[@]};${GENOMES}
