c3de5caa65576e2a25103fb543bf33a70304ef1f
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-06-06 16:58:22 +1000

    GENOMES array fixed

--- Script_One_prokka.sh
+++ Script_One_prokka.sh
@@ -9,13 +9,16 @@ Mkdir prokka_outputs
 # Navigate to the directory with the input .fasta files. Prokka automatically uses the files contained in the directory you run thr program from, as input.
 Cd genomes/
 
+#Define Genomes as an array (had success with the following on Mac from /Users/polly)
+GENOMES=(`ls Documents/Github/Phylogenomic-study/Genomes/*.fasta`) 
+
 #Define 'file' as an array
 #basename is a command used to strip suffix information from a filename, which leaves only the file name itself.
-File= (`basename ${input} .fasta`) 
+File=(`basename ${INPUT} .fasta`) 
 
 #Set up loop to go through each FASTA file and run prokka 
-For File in *.fasta; do
+for File in *.fasta ; do \
 
 #Script to run prokkka on all megalocytivirus and iridovirus genomes
 #If an out directory using --outdir isn't specified Prokka will put the out directory in the same directory as the input files 
-% prokka --kingdom viruses --outdir /Desktop/MCV_pipeline/Prokka_outputs/${File[@]} --prefix ${File[@]} --locustag ${file[@]};${GENOMES}
+prokka --kingdom viruses --outdir /Desktop/MCV_pipeline/Prokka_outputs/${File} --prefix ${File} --locustag ${File} ; done
