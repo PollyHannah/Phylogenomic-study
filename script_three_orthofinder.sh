6546a7ca6ecb63f4200159afa8f119ea26aadfc5
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-07-15 17:24:49 +1000

    Updated script to add -t and -a options to speed up analysis
    A description of, and reason for adding,  each option, can be found in the .sh file.

--- script_three_orthofinder.sh
+++ script_three_orthofinder.sh
@@ -11,13 +11,15 @@
 # '-T option allows the user to choose the tree inference program used to make gene trees'
 #'-ot' option asks Orthofinder to Stop the program running after gene trees have been inferred.
 # -z OrthoFinder automatically trims alignments (find more detail at https://github.com/davidemms/OrthoFinder). This option stops the trimming in order to 'sanity check' the alignment before the gene trees are produced based on the alignments.
+# -t Specifies the number of processes which are done in parallel for the BLAST/DIAMOND searches and tree inference steps. It is advised that you use as many threads as there are cores available on your computer, which is the default if another value is not specified (as in here).
+# -a Specified the number of threads for the BLAST/DIAMOND steps. If the '-a' parameter is not specified (as is not done here) it will default to 16 or one eighth of the '-t' parameter (whichever is smaller), which is reccomended by the Orthofinder developer.
 #'-f' option allows the user to indicate which directory of FASTA files Orthfinder should start the analysis from
 # '-o' option allows the user to indicate the directory the results files should be saved to.
 
 #Do not re-create a Directory for the Orthofinder results. Orthofinder creates the output folder as part of the script below.
 
 #Run Orthofinder on proteomes
-orthofinder -p ./tempdir -M msa -T iqtree -ot -z -f ./prokka_outputs/proteome -o ./orthofinder
+orthofinder -p ./tempdir -M msa -T iqtree -ot -z -t -a -f ./prokka_outputs/proteome -o ./orthofinder
 
 
 #If running on Rob's server using 14 CPUs change to: orthofinder -t 14 -a 14 -p /data/polly/megalocytivirus-project/tempdir -M msa -T iqtree -ot -f /data/polly/megalocytivirus-project/Proteome -o /data/polly/megalocytivirus-project/Orthofinder
