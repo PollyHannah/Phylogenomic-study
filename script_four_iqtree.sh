b551568d031da82063524a937eac1d3950a65e97
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-07-23 10:56:40 +1000

    Updated iqtree script to run in parallel to reduce computational effort.
    Other fixes include updating the script to generate a gene tree for each MSA input file, as opposed to one big tree which includes all MSA input files. This was a mistake in the inital script which is now fixed.

--- script_four_iqtree.sh
+++ script_four_iqtree.sh
@@ -1,15 +1,36 @@
+# The modules to load:
+module load iqtree/2.2.0.5
+module load parallel
+
 # Make a directory for the iqtree outputs
 mkdir -p iqtree
 
-# The modules to load:
-module load iqtree/2.2.0.5
+# Establish files directory for input into parallel n
+files=$(ls path/to/MultipleSequenceAlignments/*.fa)
 
+# Establish input and output directories
+input_dir="path/to/MultipleSequenceAlignments"
+output_dir="iqtree"
+
+# Run iqtree
 #This script takes Multiple Sequence Alignment (MSA) .fa files as input and reconstructs a maximum-likelihood (ML) tree for each orthogroup. It will use ModelFinder to select the best-fit model to reconstruct the ML tree, and access branch supports using UFBoot (or 'UltraFast bootstrap').
-
 # -s option is used to specify the input alignment file or a directory of files.
 # -B to specify the number of replicates for the bootstraping
 # -T is to determine the number of CPU cores to speed up the analysis.
 # -m MFP option specifies to IQTREE to do an extended model selection followed by tree inference.
-# -pre option specifies the output directory. 
+# -pre option specifies the output directory / prefix of the output file names. 
 
-iqtree2 -s orthofinder_iqtreetest/Results_Jul15/MultipleSequenceAlignments -m MFP -B 1000 -T AUTO -pre iqtree/
+#Create function
+run_iqtree() {
+    local file=$1
+    local basefile=$(basename "$file".fa)
+    iqtree2 -s "$file" -B 1000 -m MFP -T 1 -pre "$output_dir/$basefile"
+}
+
+#Export function and input and output directories to be used by parallel. 
+export -f run_iqtree
+export input_dir
+export output_dir
+
+# Run IQTREE2 on each file in parallel
+parallel -j ${SLURM_NTASKS_PER_NODE} run_iqtree ::: "${files[@]}"
