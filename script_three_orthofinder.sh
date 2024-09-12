5c594c6b44b736828e2855ef163328d86c03f7b1
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-09-12 10:20:01 +1000

    Update script_three_orthofinder.sh

--- script_three_orthofinder.sh
+++ script_three_orthofinder.sh
@@ -12,11 +12,11 @@
 # -z OrthoFinder automatically trims alignments. This option stops the trimming in order to 'sanity check' the alignment before the gene trees are produced based on the alignments.
 # -t Specifies the number of processes which are done in parallel for the BLAST/DIAMOND searches and tree inference steps. It is advised that you use as many threads as there are cores available on your computer, which is the default if another value is not specified (as in here).
 # -a Specified the number of threads for the BLAST/DIAMOND steps. If the '-a' parameter is not specified (as is not done here) it will default to 16 or one eighth of the '-t' parameter (whichever is smaller), which is reccomended by the Orthofinder developer.
-#'-f' option allows the user to indicate which directory of FASTA files Orthfinder should start the analysis from
+#'-f' option allows the user to indicate which directory of FASTA files Orthfinder should start the analysis from.
 # '-o' option allows the user to indicate the directory the results files should be saved to.
 
 # Do not re-create a Directory for the Orthofinder results. Orthofinder creates the output folder as part of the script below.
 # You will need to create the 'tempdir' directory.
 
 #Run Orthofinder on proteomes
-orthofinder -p ./tempdir -M msa -ot -z -t 24 -a 6 -f ./prokka_outputs/proteome -o ./orthofinder
+orthofinder -p ./tempdir -M msa -ot -z -t 24 -a 6 -f ./prokka_outputs/1_prokka-o ./orthofinder
