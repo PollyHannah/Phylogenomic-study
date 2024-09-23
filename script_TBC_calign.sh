e23c5b8a8b0ddeb8bca66379b34c964ba2aaa66f
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-09-23 21:00:17 +1000

    Added CIAalign script

---
+++ script_TBC_calign.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# CSIRO Petrichor
+# Login with Nexus username and password
+# Do not run jobs directly from the login node
+# Use an interactive node: sinteractive -A <O2D_code> -t hh:mm:ss -c 1 -m 1G
+# cd $SCRATCH3DIR
+# Check job status: squeue -u username
+# Kill job: scancel <job_ID>
+# Check job efficiency: seff <job_ID>
+# Check job accounting data: sacct (CPU efficiency=TotalCPU/(AllocCPUS*Elapsed), memory efficiency=MaxRSS/ReqMem)
+
+# CSIRO project code
+#SBATCH --account=OD-220983
+
+# The name of the job:
+#SBATCH --job-name="2024-09-19_calign_script_test"
+
+# SMP processes must run as one task on one node
+# Do not request multiple nodes unless appropriate and necessary
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+#Lachlan has designated maximum allocations below. Ensure you revise these allocations prior to submitting a job. 
+
+# Maximum number of CPU cores to be used by the job:
+#SBATCH --cpus-per-task=12
+
+# Maximum memory to be used by the job:
+#SBATCH --mem=1G
+# Default memory units are megabytes - specify [K|M|G|T]
+
+# Send an email when the job:
+# aborts abnormally (fails)
+#SBATCH --mail-type=FAIL
+# begins
+#SBATCH --mail-type=BEGIN
+# ends successfully
+#SBATCH --mail-type=END
+
+# Use this email address:
+#SBATCH --mail-user=polly.hannaford@csiro.au
+
+# The maximum running time of the job in DD-hh:mm:ss
+#SBATCH --time=00-01:00:00
+
+#This script uses CAlign to process (clean) Multiple Sequence Alignment Files. 
+
+#The program removes columns containing only gaps. This function is run by default, to not run this function specify.
+# --infile is the path to the input file being a multiple sequence alignment file in fasta format (.fasta). 
+# -outfile_stem is a prefix for the output files.
+# --crop_ends crops the ends of individual sequences if they contain a high proportion of gaps relative to the rest of the alignment.
+# CIAlign --help give you help with your options for functions.
+
+CIAlign --infile G0000000_manual.fasta --outfile_stem G0000000_manual_3 --crop_ends --remove_insertions
+
