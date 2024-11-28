bebe0af5a6711e86cca865849304765774a2e0b1
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-11-28 12:48:38 +1100

    Clearing this repository to re-write it.
    Clearing my Github repository to re-write my project, .readme file, and outputs.

--- script_two_collect_proteome_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/Bash
-
-#Make a Directory called proteome into which you can save all annotated proteomes. The '-p' option stands for "parent" and it allows you to create a directory hierarchy (a tree of directories) in one step. 
-
-mkdir -p ./prokka_outputs/1_prokka
-
-#Copy all proteome file outputs (which end in .faa) from Prokka for each genome, found in each of the Prokka output folders for each genome, into the newly created directory '1_prokka'.
-
-cp ./prokka_outputs/*/*.faa ./prokka_outputs/1_prokka
