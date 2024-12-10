6422dae377f7e9367c5961e35cf3807f5d190e38
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2024-12-10 16:37:13 +1100

    Update script_three_orthofinder.sh
    Updated script to include -M msa option as i remembered it's useful to look at the multiple sequence alignment of orthogroups when quality checking annotations and genomes. 
    
    I saw the -z option was missing so I re-added that. I changed the option -og to -ot to keep OrthiFinder running until multiple sequence alignments are generated.

--- script_three_orthofinder.sh
+++ script_three_orthofinder.sh
@@ -8,7 +8,8 @@
 # Trees can be inferred using multiple sequence alignments (MSA) by using the option '-M msa' option. MAFFT is used to generate the MSAs as default.
 # OrthoFinder infers orthologs from rooted gene trees. However, tree interence methods create unrooted gene trees/ OrthoFinder requires a rooted species tree to root the gene trees before ortholog inference can begin. The default species tree method is STAG.
 # '-p' option houses the temporary 'pickles files' into an allocated directory.
-# '-og' stops program after inferring orthogroups.
+#'-M msa' option asks Orthofinder to make multiple sequence alignments. The default program OrthoFinder uses is MAFFT.
+#'-ot' option asks Orthofinder to stop the program running after gene trees have ben inferred for Othogroups.
 # -z OrthoFinder automatically trims alignments. This option stops the trimming in order to 'sanity check' the alignment before the gene trees are produced based on the alignments.
 # -t Specifies the number of processes which are done in parallel for the BLAST/DIAMOND searches and tree inference steps. It is advised that you use as many threads as there are cores available on your computer, which is the default if another value is not specified (as in here).
 # -a Specified the number of threads for the BLAST/DIAMOND steps. If the '-a' parameter is not specified (as is not done here) it will default to 16 or one eighth of the '-t' parameter (whichever is smaller), which is reccomended by the Orthofinder developer.
@@ -22,4 +23,4 @@
 module load orthofinder
 
 #Run Orthofinder on proteomes
-orthofinder -p ./tempdir -og -t 24 -a 6 -f ./proteome_1 -o ./orthofinder_1
+orthofinder -p ./tempdir -M msa -z -ot -t 24 -a 6 -f ./proteome_1 -o ./orthofinder_1
