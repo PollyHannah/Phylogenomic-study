e807c810e0471bf7954197d6778e1eacf47a9f83
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2025-01-30 13:37:16 +1100

    Update

--- classification/script_fastani.sh
+++ classification/script_fastani.sh
@@ -1,7 +1,5 @@
 # FastANI to compare the sequence similarity between a set of reference and set of query genomes.
-
-#load module 
+# load module 
 module load fastani
-
 # Run FastANI 
 fastANI --ql query_list.txt --rl reference_list.txt -o fastani.out
