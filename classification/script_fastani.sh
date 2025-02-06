6181de23b84f564fe8ad92a67a1aaf7b3229163f
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2025-02-06 13:17:29 +1100

    Uploading two novel ISKNV genomes following annotation check process.

--- classification/script_fastani.sh
+++ classification/script_fastani.sh
@@ -1,5 +1,5 @@
 # FastANI to compare the sequence similarity between a set of reference and set of query genomes.
 # load module 
-module load fastani
+module load fastANI
 # Run FastANI 
 fastANI --ql query_list.txt --rl reference_list.txt -o fastani.out
