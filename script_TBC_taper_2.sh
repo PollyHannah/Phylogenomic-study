e0d398afabb8eac1bb63dbf5e345b69a91633088
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2025-03-04 15:22:34 +1100

    Removed TAPER scripts (second version) as updated version does not make any difference to output.

--- script_TBC_taper_2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# job commands
-./julia-1.11.3/bin/julia correction_multi.jl -l files2run_family.txt 
-./julia-1.11.3/bin/julia correction_multi.jl -l files2run_genus.txt 
-./julia-1.11.3/bin/julia correction_multi.jl -l files2run_species.txt
