af94bfa57aa02e26cb1c448f807e1e14ebb9fad5
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2025-05-09 17:03:28 +1000

    Updates after including new megalocytivirus genome European chub iridovirus

--- classification/script_fastani.sh
+++ classification/script_fastani.sh
@@ -1,5 +1,5 @@
 # FastANI to compare the sequence similarity between a set of reference and set of query genomes.
 # load module 
-module load fastANI
+module load fastani
 # Run FastANI 
 fastANI --ql query_list.txt --rl reference_list.txt -o fastani.out
