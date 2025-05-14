914fe5619d8af4d494e376f513ae6059583ecfa2
PollyHannah <160091608+PollyHannah@users.noreply.github.com>
2025-05-14 19:26:44 +1000

    Updates following review of alignments and trees with sequences included for newly-added genome of European chub iridovirus

--- script_TBC_review_genus_trees.sh
+++ script_TBC_review_genus_trees.sh
@@ -5,7 +5,7 @@ dir="iqtree_genus_trees"
 
 # Loop over each file in the directory
 for file in "$dir"/*; do
-    if grep -q 'TSIV' "$file" || grep -q 'SDDV' "$file"; then
+    if grep -q 'TSIV' "$file" || grep -q 'SDDV' "$file" || grep -q 'ECIV' "$file"; then
         echo "$(basename "$file") FALSE"
     else
         echo "$(basename "$file") TRUE"
